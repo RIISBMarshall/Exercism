d90a17f036e197217542b10ad341e32305c0dcf3
Brian Marshall <bmarshall@RIIS.com>
2017-03-10 09:39:02 -0500

    Changed code for GradeSchool.swift

--- grade-school/Sources/GradeSchool.swift
+++ grade-school/Sources/GradeSchool.swift
@@ -1,21 +1,21 @@
-import Foundation
-
-class GradeSchool{
-    
-    var roster = [Int: [String]]()
- 
-    func addStudent(_ studentName: String, grade: Int){
-        if(roster[grade]?.isEmpty == nil){
-            roster[grade] = [studentName]
-        }else{
-            roster[grade]?.append(studentName)
-        }
-    }
-    
-    func studentsInGrade(_ grade: Int) -> [String]{
-        if(roster[grade]?.isEmpty == nil){
-            return [String]()
-        }
-        return roster[grade]!
-    }
+struct GradeSchool
+{
+   var roster: [Int: Set<String>] = [:]
+   
+   mutating func addStudent(_ name: String, grade: Int)
+   {
+      var rosterGrade = roster[grade] ?? []
+      rosterGrade.insert(name)
+      roster.updateValue(Set(rosterGrade.sorted()), forKey: grade)
+   }
+   
+   func studentsInGrade(_ grade: Int) -> Set<String>
+   {
+      return roster[grade] ?? Set<String>()
+   }
+   
+   var sortedRoster: [Int: Set<String>] {
+      return roster
+   }
 }
+
